49f63cb58bae46535265836580c8da691b3d9e9a
Phil Pirozhkov <pirjsuka@gmail.com>
2014-09-24 12:35:17 +0400

    Update to work with redmine 2.5.1

--- init.rb
+++ init.rb
@@ -1,9 +1,9 @@
 require 'redmine'
 
-IssuesHelper.module_eval do
-  alias csv_with_bom_issues_to_csv_orig issues_to_csv
-  def issues_to_csv(*args)
-    ret = csv_with_bom_issues_to_csv_orig(*args)
+QueriesHelper.module_eval do
+  alias query_to_csv_orig query_to_csv
+  def query_to_csv(*args)
+    ret = query_to_csv_orig(*args)
     if l(:general_csv_encoding) == "UTF-8"
       ret = "\xEF\xBB\xBF".force_encoding("UTF-8")+ret
     end
@@ -13,9 +13,9 @@ end
 
 Redmine::Plugin.register :redmine_csv_with_bom do
   name 'Redmine CSV with BOM plugin'
-  author 'TOMITA Masahiro, SERGEY Prosin'
+  author 'TOMITA Masahiro, SERGEY Prosin, Phil Pirozhkov'
   description 'This is a plugin for Redmine to prepend BOM to CSV'
-  version '0.0.2'
-  url 'http://github.com/tmtm/redmine_csv_with_bom'
-  author_url 'http://github.com/tmtm'
+  version '0.0.3'
+  url 'http://github.com/pirj/redmine_csv_with_bom'
+  author_url 'http://github.com/pirj'
 end
